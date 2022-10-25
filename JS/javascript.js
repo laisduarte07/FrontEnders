c5e192bef2ebf846a4576f59db02e234d192ad70
Lais Duarte <laisbenvindo@gmail.com>
2022-10-24 22:01:14 -0300

    alteração nas cores, troca da imagem e 1 icone e adc alert no js de recup

--- JS/javascript.js
+++ JS/javascript.js
@@ -4,3 +4,10 @@ const validaEmail = (email) => {
     const re = /\S+@\S+\.\S+/;
     return re.test(email);
 };
+ 
+
+/* Botão enviar - página de Recuperação */
+
+function enviar(){
+    alert('Email enviado')
+}
