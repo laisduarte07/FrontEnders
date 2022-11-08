363d648c8feae875aea286595ad42fc60f34fda1
MaliceBalbino <113525971+MaliceBalbino@users.noreply.github.com>
2022-11-08 12:09:31 -0300

    atualizado

--- formulario.js
+++ formulario.js
@@ -10,132 +10,77 @@ const neighborhoodInput = document.querySelector ("#neighborhood");
 const regionInput = document.querySelector ("#region");
 const formInputs = document.querySelectorAll ("[data-input]");
 
-  
+   
 const closeButton = document.querySelector ("#close-message");
 
 const fadeElement = document.querySelector ("#fade");
 
+
 // Validate CEP input
 
 cepInput.addEventListener ("keypress", (e) => {
-
-const onlyNumbers = /[0-9]/;
-
-const key = String.fromCharCode(e.keyCode);
-
-// allow only numbers 
-
-if(!onlyNumbers.test (key)) {
-    e.preventDefault();
-    return;
-}
-});
-
-// Get adress event
-
-cepInput.addEventListener ("keyup", (e) => {
-
+    const onlyNumbers  = /[0-9]/;
     const inputValue= e.target.value 
 
-    //check if we have the correct length
-    if (inputValue.length === 8) {
-        getAddress(inputValue);
+    const key = String.fromCharCode (e.keyCode);
+
+
+    // allow only numbers
+
+    console.log (e.keyCode);
+    console.log (key);
+    if(!onlyNumbers.test(key)) {
+        e.preventDefault () 
+        return;
     }
 });
 
-// Get costumer address from API
+//Get address event
+
+cepInput.addEventListener ("keyup",  (e) => {
+
+    const inputValue = e.target.value;
+
+    //check if we have the correct length
+
+     if (inputValue.length === 8) {
+        getAddress(inputValue);
+    }
+  
+});
+
+// Get custumer address from API
+
 const getAddress = async (cep) => {
-      toggleLoader();
-      cepInput.blur();
+    
+    cepInput.blur();
 
-      
-    const apiUrl = 'https://viacep.com.br/ws/${cep}/json/'
+    const apiUrl = `http:viacep.com.br/ws/${cep}/json/`;
 
-    const response = await fetch (apiUrl);
+    const response = await fetch(apiUrl);
 
     const data = await response.json();
 
-
-    // show error and reset form
-
-    if (data.erro === "true") {
-        if(!addressInput.hasAttribute ("disabled")) {
-            toggleDisabled();
-        }
-
-        addressForm.reset(); 
-        toggleLoader();
-        toggleMessage("CEP inválido, tente novamente.")
-    return;
-    }
-    
-    if(addressInput.value === "") {
-        toggleDisabled();
-    }
-    
     addressInput.value = data.logradouro
     cityInput.value = data.localidade
-    neighborhoodInput.value =data.bairro
+    neighborhoodInput.value = data.bairro
     regionInput.value = data.uf 
-    toggleLoader(); 
+ };
 
-};
+ 
 
 
 // Add or remove disabled atributte
 
-    const toggleDisabled = () => {
-        if (regionInput.hasAttribute ("disabled")) {
-        formInputs.forEach ((input) => {
-            input.removeAttribute ("disabled");
-        })
-        } else {
-        formInputs.forEach ((input) => {
-         input.setAttribute ("disabled", "disabled");
-        });
-    }
-    };
-
-// show or hide loader
-
-const toggleLoader = () => {
-    const fadeElement = document.querySelector ("#fade");
-    const loaderElement = document.querySelector ("#loader");
-
-    fadeElement.classList.toggle("hide");
-    loaderElement.classList.toggle ("hide");
-}; 
-
-// show or hide message
-
-const toggleMessage = (msg) => {
-    const messageElement = document.querySelector ("#message");
-    const messageElementText = document.querySelector ("#message p");
-   
-    messageElementText.innerText = msg;
-
-    fadeElement.classList.toggle ("#hide");
-    messageElement.classList.toggle ("#hide");
-
+const toggleDisabled = () => {
+    if (regionInput.hasAttribute ("disabled")) {
+    formInputs.forEach ((input) => {
+        input.removeAttribute ("disabled");
+    })
+    } else {
+    formInputs.forEach ((input) => {
+     input.setAttribute ("disabled", "disabled");
+    });
+}
 };
 
-// close message model 
-
-closeButton.addEventListener ("click", () => toggleMessage ());
-
-// save address
-addressForm.addEventListener ("submit", (e) => {
-    e.preventDefault()
-
-    toggleLoader()
-
-    setTimeout(() => {
-        toggleLoader () 
-        toggleMessage ("Dados salvos com Sucesso")
-
-        addressForm.reset();
-        toggleDisabled();
-         
-
-    }, 1500)    
-});
